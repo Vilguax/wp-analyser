8314c8828947dc5c732e52298416587bb1c3a499
Vilguax <axel.pelassa@hotmail.fr>
2023-07-03 16:00:46 +0200

    ajout du style + contenut wpanalyser

---
+++ wp-analyser.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+    Plugin Name: WP Analyser
+    Plugin URI: http://www.wp-analyser.com
+    Description: This plugin will analyse your wordpress installation and provide you with a report on how to improve your wordpress installation.
+    Version: 1.0
+    */
